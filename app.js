1d4f829e43d3d5e5f62221830e67b77bd69ad3ff
Juan Diego <jvega_18@alu.uabcs.mx>
2024-04-24 09:52:40 -0700

    primer commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+let data = [{name:'ana'},{name:'juan'},{name:'maria'}]
+
+app.use(express.json())
+
+
+app.get( '/', (req, res) => {
+
+    res.send('bienvenido a mi app')
+})
+
+app.get( '/users', (req, res) => {
+
+    res.send(data)
+})
+
+
+// METODOS HTTP
+
+app.post( '/users', (req, res) => {
+    console.log('peticion POST con body');
+    data.push (req.body)
+    res.send('datos guardados correctamente')
+
+})
+
+app.put( '/users/:id', (req, res) => {
+
+    const id = req.params.id
+    data[id] = req.body
+    res.send('datos modificados correctamente')
+})
+
+app.delete( '/users/:id', (req, res) => {
+
+   const id = req.params.id
+   data.splice(id,1)
+   res.send('datos eliminados correctamente')
+})
+
+app.patch('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const newData = req.body;
+    Object.assign(data[id], newData);
+    res.send('Datos actualizados correctamente');
+});
+
+
+app.listen(PORT, () => {
+    console.log('la app corriendo en el puerto: ' + PORT)
+})
+
